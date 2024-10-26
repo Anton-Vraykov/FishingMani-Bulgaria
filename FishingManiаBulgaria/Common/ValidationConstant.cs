50a4683cb24c7e27056b1eaff2e457c2fab3c0af
antonvraykov@gmail.com <antonvraykov@gmail.com>
2024-10-26 14:27:05 +0300

    Add project files.

---
+++ FishingManiаBulgaria/Common/ValidationConstant.cs
@@ -0,0 +1,14 @@
+﻿namespace FishingManiаBulgaria.Common
+{
+    public class ValidationConstant
+    {
+        public const int PlaceNameMax = 100;
+        public const int PlaceNameMin = 3;
+        public const int PlaceLocationMax = 100;
+        public const int PlaceLocationMin = 3;
+        public const int PlaceDescriptionMax = 100;
+        public const int PlaceDescriptionMin = 3;
+
+
+    }
+}
---
+++ FishingManiаBulgaria/Data/Models/FishingPlace.cs
@@ -0,0 +1,25 @@
+﻿using FishingManiаBulgaria.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace FishingManiаBulgaria.Data.Models
+{
+    public class FishingPlace
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(ValidationConstant.PlaceNameMax)]
+        public string Name { get; set; }=string.Empty;
+        [Required]
+        public string PictureURL { get; set; }= string.Empty;
+        [Required]
+        [MaxLength(ValidationConstant.PlaceLocationMax)]
+        public string Location { get; set; }
+        public DateTime Reservation { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
